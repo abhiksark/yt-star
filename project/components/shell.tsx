dfed3492a5d11f9185fdd1f42f4b68e28d4eed83
Abhik <abhiksark@gmail.com>
2024-12-04 17:22:09 +0530

    Remove unused files and components; clean up project structure

--- project/components/shell.tsx
+++
@@ -1,9 +0,0 @@
-interface ShellProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function Shell({ children, className, ...props }: ShellProps) {
-  return (
-    <div className="container max-w-7xl mx-auto p-4 md:p-6 lg:p-8" {...props}>
-      {children}
-    </div>
-  );
-}
